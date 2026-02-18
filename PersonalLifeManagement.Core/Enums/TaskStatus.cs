dae916bd7accbda0a28163fbf0138ecf4c3fb605
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2026-02-18 12:47:50 -0800

    create enum for task status

---
+++ PersonalLifeManagement.Core/Enums/TaskStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Enums
+{
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// باید انجام داده شود
+        /// </summary>
+        Todo = 0,
+
+        /// <summary>
+        /// در حال انجام
+        /// </summary>
+        Doing = 1,
+
+        /// <summary>
+        /// تمام شده
+        /// </summary>
+        Done = 2
+    }
+}
