27155891c23dd19d77fb01a1fd60cbe354e2cd96
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2026-02-20 03:59:00 -0800

    create Goal Model

---
+++ PersonalLifeManagement.Core/Entities/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Goal : BaseEntity         // target
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+
+        protected Goal() { }
+
+        public Goal(string title, string? description, DateTime startDate, DateTime? endDate)
+        {
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
